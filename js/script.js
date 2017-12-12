96f80fdcb59879a7199b286d8e063fcaaa0f0330
Sergey2782 <msergun234@gmail.com>
2017-12-12 18:28:58 +0300

    refactoring my code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
-//-----    custom slider on clean JS ----------//
+                //-----    custom slider on clean JS ----------//
 
-//------  variables   ----//
+                          //------  variables   ----//
+
 var img = document.querySelector('#show'),
     img_small_s = document.querySelectorAll('.showSmall'),
     btn_forward = document.querySelector('.btn_forward'),
-    btn_back = document.querySelector('.btn_back');
-  //-------  events  -------//
+    btn_back = document.querySelector('.btn_back'),
+    main = 4,
+    prev = 3,
+    next = 5,
+    img_len = img_small_s.length;
+  
+                         //-------  events    -------//
+
     img.onload =  firstMainImageOnLoad();
-    btn_forward.onclick = showNextImage;
-    btn_back.onclick = showPrevImage;
- //-----  function for the correct loading FIRST images on the web-page --------//
+    btn_back.onclick = function() {switch_Image('minus')};
+    btn_forward.onclick = function() {switch_Image('plus')};
+   
+ //-----  function for the correct loading FIRST images on the web-page ---//
+
   function firstMainImageOnLoad(){
          var dataSrc = img.getAttribute('data-src');
          img.src = dataSrc;//---- assign to 'src' data from 'data-src'    
  }
- //----- beautiful  animation-effect  to change image -----//
+
+ //-----  function for beautiful  animation-effect  for change images -----//
+
    function MyFadeFunction() {
          if (opacity<1) {
              opacity += .03;
@@ -22,96 +33,68 @@ var img = document.querySelector('#show'),
          }
              img.style.opacity = opacity;
      }
- //---- show previous images on click -----//
-  var main = 4;
-  var prev = 3;
-  var next = 5;
- 
-    function showPrevImage(){
-         opacity = 0;
-         MyFadeFunction();
- 
-         main --;
-         prev --;
-         next --;
-        
-        if(main < 0){
-             main = img_small_s.length - 1; //--- when going to the first image, go to the last one
-           };
-        if(prev < 0){
-             prev = img_small_s.length - 1; //--- when going to the first image, go to the last one
-           };
-        if(next < 0){
-             next = img_small_s.length - 1; //--- when going to the first image, go to the last one
-           };
-        
+  
+//--------------     function - select images for showing on page  --------//
+
+ function selectImgAndShow (){
         img.setAttribute('src', img_small_s[main].getAttribute('data-src') );
-        
+
     var img_small = img_small_s[prev];
         img_small.setAttribute('src', img_small.getAttribute('data-src') );
         img_small.style.display = 'inline-block';
-        
+
         img_small = img_small_s[next];
         img_small.setAttribute('src', img_small.getAttribute('data-src') );
         img_small.style.display = 'inline-block';
-         
+
         img_small = img_small_s[main]; 
         img_small.setAttribute('src', img_small.getAttribute('data-src') );
         img_small.style.display = 'inline-block';
-        
-         for(var i = 0; i < img_small_s.length; i ++ ){
-                    if(i == prev  || i == main || i == next){
-                        img_small_s[i].style.display = 'inline-block';
-                    }else {
-                        img_small_s[i].style.display = 'none';
-                    }
-         }
-}
- //---- show next images on click -----//
-    function showNextImage(){
+
+             for(var i = 0; i < img_len; i ++ ){
+                        if(i == prev  || i == main || i == next){
+                            img_small_s[i].style.display = 'inline-block';
+                        }else {
+                            img_small_s[i].style.display = 'none';
+                        }
+             }
+ }
+
+ function switch_Image(sym){
          opacity = 0;
          MyFadeFunction(); 
- 
-         main ++;
-         prev ++;
-         next ++;
-        
-        if(main > img_small_s.length - 1){
-             main = 0; 
-           };
-        if(prev > img_small_s.length - 1){
-             prev = 0;
-           };
-        if(next > img_small_s.length - 1){
-             next = 0;
-           };
-        
-        img.setAttribute('src', img_small_s[main].getAttribute('data-src') );
-        
-    var img_small = img_small_s[prev];
-        img_small.setAttribute('src', img_small.getAttribute('data-src') );
-        img_small.style.display = 'inline-block';
-        
-        img_small = img_small_s[next];
-        img_small.setAttribute('src', img_small.getAttribute('data-src') );
-        img_small.style.display = 'inline-block';
-        
-        
-        img_small = img_small_s[main]; 
-        img_small.setAttribute('src', img_small.getAttribute('data-src') );
-        img_small.style.display = 'inline-block';
-        
-         for(var i = 0; i < img_small_s.length; i ++ ) {
-             
-                     if(i == prev  || i == main || i == next) {
-                        img_small_s[i].style.display = 'inline-block';
-                    } else {
-                        img_small_s[i].style.display = 'none';
-                    }
-         }
-        
+
+         if(sym == 'minus'){
+               
+                main --; prev --; next --;
+
+                if(main < 0){
+                     main = img_len - 1;
+                   };
+                if(prev < 0){
+                     prev = img_len - 1;
+                   };
+                if(next < 0){
+                     next = img_len - 1;
+                   };
+                    selectImgAndShow();
+         }else{ 
+
+                main ++; prev ++; next ++;
+
+                if(main > img_len - 1){
+                     main = 0; 
+                   };
+                if(prev > img_len - 1){
+                     prev = 0;
+                   };
+                if(next > img_len - 1){
+                     next = 0;
+                   };
+                    selectImgAndShow();
+                };
 }
- 
+     
 
 
 
